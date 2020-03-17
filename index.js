c452aaa29a4082e720a5b37a00434698838edd53
Eli Wendlick <ewendlick@gmail.com>
2020-03-17 11:44:18 +0900

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import * as wasm from "hello-wasm-pack";
+import { Universe } from 'wasm-game-of-life';
 
-wasm.greet();
+// wasm.greet("Stringy McStringerson");
+
+const pre = document.getElementById('game-of-life-canvas')
+const universe = Universe.new()
+
+// The Javascript runs in a requestAniamtionFrame loop.
+// On each iteration, it draws the current universe to the <preP., and then calls Universe::tick
+//
+const renderLoop = () => {
+  pre.textContent = universe.render()
+  universe.tick()
+
+  requestAnimationFrame(renderLoop)
+}
+
+requestAnimationFrame(renderLoop)
